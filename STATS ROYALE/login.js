c2f619b16793c036a2da25ac46d090b195551f17
mathadri007 <142513084+mathadri007@users.noreply.github.com>
2023-11-10 10:24:13 -0300

    tudo

---
+++ STATS ROYALE/login.js
@@ -0,0 +1,18 @@
+   <script>
+        
+        function logar(){
+            event.preventDefault();
+
+            let user = document.getElementById('login').value;
+            let pass = document.getElementById('senha').value;
+
+            if(user == "golem" && pass == "golem"){
+                alert('Sucesso');
+                window.location.href = "pagina inicial.html";
+            }else{
+                alert('Usuário ou senha incorretas');
+            }
+        }
+
+
+    </script>
